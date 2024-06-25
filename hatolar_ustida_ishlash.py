40a85fe49d0d13937efeb13f4af864987efe83b1
shohrux2011 <shoxruh449@gmail.com>
2024-06-26 01:13:46 +0500

    Create hatolar_ustida_ishlash.py
    bu yerda Mening hatolarim ustida ishlaganim

---
+++ hatolar_ustida_ishlash.py
@@ -0,0 +1,93 @@
+# # print "Hello World!" SyntaxError
+# # print("Hello World! SyntaxError
+# # print("Hello World!"  SyntaxError
+# # print("Hello World!") #togri
+# # print("Hello World!") #togri
+
+# # IndentationError
+# # print("O'ngacha sanaymiz")
+# # for n in range(10):
+# # print(n+1)
+# print("O'ngacha sanaymiz")#togri
+# for n in range(10):#togri
+#     print(n+1)#togri
+    
+# # IndentationError
+# # son = 50
+# # if son>=0:
+# #     print("Musbat son")
+# # else:
+# # print("Manfiy son")
+# son = 50#togri
+# if son>=0:#togri
+#     print("Musbat son")#togri
+# else:#togri
+#     print("Manfiy son")#togri
+ 
+# # TypeError    
+# # son = input("Istalgan son kiriting: ")
+# # print(f"{son} ning kvadrati {son**2} ga teng")
+# son = float(input("Istalgan son kiriting: "))#togri
+# print(f"{son} ning kvadrati {son**2} ga teng")#togri
+
+# # NameError
+# # prit("Hello World!")
+# print("Hello World!")#togri
+# # mevalar = ['olma','uzum','nok','anor','anjir']
+# # for meva in mvealar:
+#     # print(meva)
+# mevalar = ['olma','uzum','nok','anor','anjir']  #togri
+# for meva in mevalar:#togri
+#     print(meva)#togri
+    
+# # ValueError
+# # son = int(input("Istalgan son kiriting: "))
+# # if son>=0:
+# #     print("Musbat son")
+# # else:
+# #     print("Manfiy son")
+# son = float(input("Istalgan son kiriting: "))
+# if son>=0:
+#     print("Musbat son")
+# else:
+#     print("Manfiy son")
+
+# IndexError
+# mevalar = ['olma','anor','uzum']
+# print(mevalar[3])
+# mevalar = ['olma','anor','uzum']   #togri
+# print(mevalar[2])    #togri
+
+# ZeroDivisionError
+# x, y = 50, 50
+# z = 250/(x-y)
+# x, y = 50, 75    #togri
+# z = 250/(x-y)        #togri
+
+# MANTIQIY XATOLAR
+# radius = 5
+# pi = 4.14
+# aylana_yuzi = pi*radius**2
+# print(aylana_yuzi)
+# radius = 5  #togri
+# pi = 4  #togri
+# aylana_yuzi = pi*radius**2  #togri
+# print(aylana_yuzi)  #togri
+
+# MANTIQIY XATOLAR
+# son = float(input("Istalgan son kiriting: "))
+# ildiz = son**1/2
+# print(f"{son} ning ildizi {ildiz} ga teng")
+# son = float(input("Istalgan son kiriting: "))   #togri
+# ildiz = son**(1/2)   #togri
+# print(f"{son} ning ildizi {ildiz} ga teng")  #togri
+
+# mevalar = ['olma','uzum','nok','anor','anjir']
+# for meva in mevalar:
+#     print(meva)
+#     print("Dastur tugadi")
+# mevalar = ['olma','uzum','nok','anor','anjir']  #togri
+# for meva in mevalar:     #togri
+#     print(meva)  #togri
+
+# print("Dastur tugadi")   #togri
