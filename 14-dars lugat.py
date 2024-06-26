d1349d7b9f3496daf1f89b68734a898f8dc93f20
shohrux2011 <shoxruh449@gmail.com>
2024-06-27 01:08:14 +0500

    Create 14-dars lugat.py
    bu mening 14-darsim lugatlar bilan ishlash

---
+++ 14-dars lugat.py
@@ -0,0 +1,44 @@
+car_0 = {'model': "ferari", 'rang': "qzil"}
+# print(car_0['model'])
+# print(car_0['rang'])
+ 
+mevalar = {'olma': 10000, 'banan': 24000, 'qulpunay': 50000}
+# print(f"Olma 1kg narxi {mevalar['olma']} so'm")
+
+talaba_0 = {'ism': "murod", 'yosh': 24, 't_yil': 2000}
+# print(f"{talaba_0['ism'].title()}, \
+#     {talaba_0['yosh']} yoshda, \
+#     {talaba_0['t_yil']}-yilda tug`ilgan")
+
+# talaba_0['kurs'] = 4
+# talaba_0['fakultitet'] = 'informatika'
+# # print(talaba_0)
+# talaba_0['ism'] = 'abulloh'
+# print(talaba_0)
+
+talaba_1 = {}
+# print(talaba_1)
+talaba_1['ism'] = 'temur abdumuxtorov'
+talaba_1['kurs'] = 2
+talaba_1['yosh'] = 20
+# print(talaba_1)
+# print(f"Talaba {talaba_1['ism'].title()} {talaba_1['kurs']}-kurs")
+talaba_1['kurs'] = 3
+# print(f"Talaba {talaba_1['ism'].title()} {talaba_1['kurs']}-kurs boldi")
+
+talaba_0 = {'ism': "murod", 'yosh': 24, 't_yil': 2000}
+# del talaba_0['t_yil']
+# print(talaba_0)
+
+telefonlar = {
+    'ali':'iphone x',
+    'vali':'galaxy s9',
+    'olim':'mi 10 pro',
+    'orif':'nokia 3310'
+    }
+# print(telefonlar['olim'])
+
+phone = telefonlar['ali']
+print(f"Alining telefoni {phone}")
+# phone = telefonlar['hasan']  #hato
+print(f"Hasanning telefoni {phone}")
